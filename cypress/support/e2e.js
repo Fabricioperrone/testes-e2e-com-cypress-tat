f34b4520db029d3c9adfe5657a0f8ee3f3c4c854
Fabrício Luis Perrone <suportesubzeroinformatica@gmail.com>
2023-07-06 14:25:53 -0300

    Configure cypress cloud

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,9 +1,14 @@
 
 
-Cypress.on('uncaught:exception', () => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
+Cypress.on('uncaught:exception', (err, runnable, promise) => {
+  // when the exception originated from an unhandled promise
+  // rejection, the promise is provided as a third argument
+  // you can turn off failing the test in this case
+  if (promise) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
 })
 
 import 'cypress-iframe'
