7f1b74b151b8d34146924c4564137ad57ffd5092
Fabrício Luis Perrone <suportesubzeroinformatica@gmail.com>
2023-07-06 15:02:51 -0300

    Configure cypress cloud

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,10 +1,4 @@
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-})
-
 import 'cypress-iframe'
 import 'cypress-mailosaur'
 
