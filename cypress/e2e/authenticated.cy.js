fe84971c2afe4cda880d513bd156f8fedb2e5802
Fabrício Luis Perrone <suportesubzeroinformatica@gmail.com>
2023-07-07 11:48:09 -0300

    Break lint and tests on purpose

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -38,6 +38,7 @@ describe('Scenarios where authentication is a pre-condition', () => {
   it('logs out', { tags: '@desktop-and-tablet' },  () => {
     cy.visit('/')
     cy.wait(2000)
+    
     cy.contains('.nav a', 'Logout').click()
     cy.get('#email').should('be.visible')
   })
