84e4f4a6749956d52f078827db8422edadd2289e
Fabrício Luis Perrone <suportesubzeroinformatica@gmail.com>
2023-07-07 11:52:45 -0300

    Fix lint issue

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -37,8 +37,8 @@ describe('Scenarios where authentication is a pre-condition', () => {
 
   it('logs out', { tags: '@desktop-and-tablet' },  () => {
     cy.visit('/')
-    cy.wait(2000)
-    
+    cy.wait('@getNotes')
+
     cy.contains('.nav a', 'Logout').click()
     cy.get('#email').should('be.visible')
   })
